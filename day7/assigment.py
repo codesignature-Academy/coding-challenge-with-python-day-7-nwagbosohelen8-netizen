b05332ab2dc3521fbc4996695c101f4107de8a52
nwagboso helen <nwagbosohelen8@gmail.com>
2025-11-20 18:13:34 +0100

    assignment done

---
+++ day7/assigment.py
@@ -0,0 +1,23 @@
+def add(a, b):
+    addition = a + b
+    return addition
+sum = add(2, 3)
+print(sum)
+
+def multiply(a, b):
+    multiplication = a * b
+    return multiplication
+times = multiply(2, 5)
+print(times)
+
+def power(a, b):
+    square = a ** b
+    return square
+the_square = power(2, 4)
+print(the_square)
+
+def substract(a, b):
+    subtraction = a - b
+    return subtraction
+minus = substract(2, 1)
+print(minus)
